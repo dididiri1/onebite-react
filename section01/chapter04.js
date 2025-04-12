be6538051785cd2cbd0b4a31bcfe24ebdc1a640b
dididiri1 <dididiri1@naver.com>
2025-04-12 14:50:18 +0900

    변수와 상수

---
+++ section01/chapter04.js
@@ -0,0 +1,24 @@
+// 1. 변수
+let age = 27;
+
+age = 30;
+
+// 2. 상수
+const birth = "0101";
+birth = "1234";
+console.log(birth);
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// $, _ 제외한 기호는 사용할수 없다.
+// 숫자로 시작할 수 없다.
+// 예약어를 사용할 수 없다.
+let $_name;
+
+let name1;
+
+// 4. 변수 명명 가이드
+let a = 1;
+let b = 1;
+
+let salesCount = 1;
+let refundCount = 1;
